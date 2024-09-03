ac455a0e1fa514d99bf362044428c09434290790
Sanjay Kumar Mehta <sanjaymehta7346@gmail.com>
2024-09-03 17:03:11 +0530

    adding multiply.js

---
+++ multiply.js
@@ -0,0 +1,10 @@
+// Creating first variable 
+let a = 10; 
+
+// Creating second variable 
+let b = 10; 
+let result = a * b; 
+
+// Printing the result 
+// in console 
+console.log("Result :", result); 
